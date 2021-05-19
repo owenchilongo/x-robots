9c3018832344a95d32c655c71239faaebe23fb61
Owen <ochilongo@gmail.com>
2021-05-18 23:32:23 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,6 +10,13 @@ var enemyAttack = 20;
 console.log(enemyNames);
 console.log("Array Length i.e. Number of Elements:", enemyNames.length);
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 var fight = function (enemyName) {
   // repeat and excute as long as the enemy-robot is alive
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -27,15 +34,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
-
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
+    
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+     enemyHealth = Math.max(0, enemyHealth - damage);
+     console.log(
       playerName +
         " attacked " +
         enemyName +
@@ -60,7 +68,9 @@ var fight = function (enemyName) {
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
+
     console.log(
       enemyName +
         " attacked " +
@@ -101,7 +111,8 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 60;
+      // enemyHealth = Math.floor(Math.random() * 21) + 40;
+      enemyHealth = randomNumber(40, 60);
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
